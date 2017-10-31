a2dfe556acb891ad6f99b941d2db750eeb0b4147
Shaun <shaunblacksheep@gmail.com>
2017-10-31 23:32:12 +0800

    Remove redundant objects in manifest for the time being. Added core.css for styling of main player/page. Add temporary popup.html for manifest's sake. Removed redundant code from core.js. Only 1 instance of plyr from now on. Move selection to variable instead of calling them everytime.

--- js/core.js
+++ js/core.js
@@ -1,69 +1,34 @@
-jQuery(document).ready(function($) {
-    if ($('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(8)').find('> iframe').length > 0) {
-        console.log("Found iframe");
-        var iFrameSource = $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(8) > iframe').attr('src');
-        console.log('iframe: ' + iFrameSource);
-        var match = /https:\/\/presentur\.ntu\.edu\.sg\/aculearn-idm\/v8\/studio\/embed\.asp\?modulename=(.*?)&author=(.*?)&/g.exec(iFrameSource);
-        if (match !== null) {
-            console.log(match[1] + "|" + match[2]);
-            // var title = $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(4) > strong > span > span').text().trim();
-
-            // Add download button
-            $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(4)').append('<br><a download href="https://ntucee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/media/1.mp4"><button id="bDownload" type="button">Download Video</button></a>');
-
-            // Add custom video player
-            $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(9)').append('<div class="vcontainer"><video poster="https://ntueee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/index/0.jpg" controls><source src="https://ntucee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/media/1.mp4" type="video/mp4"></video></div><div class="actions"><button type="button" class="btn js-rewind">Rewind</button><button type="button" class="btn js-forward">Forward</button></div>');
-
-            // Remove iframe video
-            $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(8)').remove();
-            var instances = plyr.setup({
-                debug: false
-            });
-
-            instances.forEach(function(instance) {
-                on('.js-rewind', 'click', function() {
-                    instance.rewind();
-                });
-
-                on('.js-forward', 'click', function() {
-                    instance.forward();
-                });
-            });
-        }
+$(document).ready(function () {
+    var videoIFrame = $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(8) > iframe');
+    if (videoIFrame.length < 0) {
+        return console.log('Video iFrame not found.');
     }
+
+    var iFrameVideoSrc = videoIFrame.attr('src');
+    var match = /https:\/\/presentur\.ntu\.edu\.sg\/aculearn-idm\/v8\/studio\/embed\.asp\?modulename=(.*?)&author=(.*?)&/g.exec(iFrameVideoSrc);
+    if (match === null) {
+        return console.log('Unable to find video src.');
+    }
+
+    // var title = $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(4) > strong > span > span').text().trim();
+
+    // Add download button
+    $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(4)').append('<br><a download href="https://ntucee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/media/1.mp4"><button id="bDownload" type="button">Download Video</button></a>');
+
+    // Add custom video player
+    $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(9)').append('<div class="vcontainer"><video poster="https://ntueee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/index/0.jpg" controls><source src="https://ntucee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/media/1.mp4" type="video/mp4"></video></div><div class="actions"><button type="button" class="btn js-rewind">Rewind</button><button type="button" class="btn js-forward">Forward</button></div>');
+
+    // Remove iframe video
+    $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(8)').remove();
+    var player = plyr.setup({
+        debug: true
+    });
+
+    $('.js-rewind').on('click', function () {
+        player[0].rewind();
+    });
+
+    $('.js-forward').on('click', function () {
+        player[0].forward();
+    });
 });
-
-var newCSS = GM_getResourceText ("customCSS");
-GM_addStyle (newCSS);
-GM_addStyle ( multilineStr (function (){/*!
-.vcontainer {
-  max-width: 800px;
-  margin: 0 auto;
-}
-.plyr {
-  border-radius: 4px;
-  margin-bottom: 15px;
-}
-*/} ) );
-
-// Get an element
-function get(selector) {
-    return document.querySelector(selector);
-}
-
-// Custom event handler (just for demo)
-function on(element, type, callback) {
-    if (!(element instanceof HTMLElement)) {
-        element = get(element);
-    }
-    element.addEventListener(type, callback, false);
-}
-
-function multilineStr (dummyFunc) {
-    var str = dummyFunc.toString ();
-    str     = str.replace (/^[^\/]+\/\*!?/, '') // Strip function () { /*!
-        .replace (/\s*\*\/\s*\}\s*$/, '')   // Strip */ }
-        .replace (/\/\/.+$/gm, '') // Double-slash comments wreck CSS. Strip them.
-    ;
-    return str;
-}
