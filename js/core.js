559c93bb628a89722fbd6058f6276e246a3b1d15
Shaun <shaunblacksheep@gmail.com>
2017-11-04 16:01:11 +0800

    iframe will now be searched instead of predefined selectors.

--- js/core.js
+++ js/core.js
@@ -1,28 +1,33 @@
 $(document).ready(function () {
-    var videoIFrame = $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(8) > iframe');
+    var videoIFrame = $('.panel-body').find('iframe');
+
     if (videoIFrame.length < 0) {
         return console.log('Video iFrame not found.');
     }
 
+    var iFrameParent = videoIFrame.parent();
     var iFrameVideoSrc = videoIFrame.attr('src');
-    // https://presentur.ntu.edu.sg/aculearn-idm/v4/opr/studioclient.asp?author=0fd405da688d6e5f74f165cdd1e5d34d&amp;modulename=studio7a79312a41dbc4191ec51641fc&amp;cat=acustudio
-    var match = /https:\/\/presentur\.ntu\.edu\.sg\/aculearn-idm\/v.*?\/studio\/embed\.asp\?modulename=(.*?)&amp;author=(.*?)&/g.exec(iFrameVideoSrc);
-    if (match === null) {
-        return console.log('Unable to find video src.');
+    var vals = url('?', iFrameVideoSrc);
+    console.log('Values: ' + JSON.stringify(vals, null, 2));
+    if (!vals.hasOwnProperty('modulename') && vals.hasOwnProperty('author')) {
+        return 'Video source has no correct paramters.';
     }
+
+    var module = vals.modulename;
+    var author = vals.author;
 
     // var title = $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(4) > strong > span > span').text().trim();
 
     // Add download button
-    $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(4)').append('<br><a download href="https://ntucee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/media/1.mp4"><button id="bDownload" type="button">Download Video</button></a>');
+    $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(4)').append('<br><a download href="https://ntucee.ntu.edu.sg/content/' + author + '/' + module + '/media/1.mp4"><button id="bDownload" type="button">Download Video</button></a>');
 
     // Add custom video player
-    $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(9)').append('<div class="vcontainer"><video poster="https://ntueee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/index/0.jpg" controls><source src="https://ntucee.ntu.edu.sg/content/' + match[2] + '/' + match[1] + '/media/1.mp4" type="video/mp4"></video></div><div class="actions"><button type="button" class="btn js-rewind">Rewind</button><button type="button" class="btn js-forward">Forward</button></div>');
+    $(iFrameParent).append('<div class="vcontainer"><video poster="https://ntueee.ntu.edu.sg/content/' + author + '/' + module + '/index/0.jpg" controls><source src="https://ntucee.ntu.edu.sg/content/' + author + '/' + module + '/media/1.mp4" type="video/mp4"></video></div><div class="actions"><button type="button" class="btn js-rewind">Rewind</button><button type="button" class="btn js-forward">Forward</button></div>');
 
     // Remove iframe video
-    $('#navcontent > div > div > div > div.panel.panel-default.panel-learner-page > div > div > div:nth-child(8)').remove();
+    videoIFrame.remove();
     var player = plyr.setup({
-        debug: true
+        debug: false
     });
 
     $('.js-rewind').on('click', function () {
